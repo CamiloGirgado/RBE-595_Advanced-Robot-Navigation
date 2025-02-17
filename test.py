f020f11171907529c1f0efeed799a38ea8ea4a6a
Camilo G <camilogirgado@gmail.com>
2025-02-17 14:20:13 -0800

    updated UCS and removed unused algos

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-a = np.array([6, 3, 5, 2, 4, 1])
-plt.plot([1, 4, 2, 5, 3, 6])
-plt.plot(a)
+#a = np.array([6, 3, 5, 2, 4, 1])
+#plt.plot([1, 4, 2, 5, 3, 6])
+#plt.plot(a)
+
+array5 = np.linspace(1/10, 1/10, 10)
+print(array5)
+
+array9 = np.ones_like(np.arange(10), dtype=float) / 10
+print(array9)
